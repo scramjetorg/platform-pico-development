d874047755b6a17e4f1dd0aa87dabd75a979c9af
Radosław Gierwiało <gierwialo@users.noreply.github.com>
2023-08-04 14:42:30 +0200

    Feature/dynamic module loading
    * Links
    
    * PoC sequence linking workflow
    
    * Docs: developer environment configuration (#1)
    
    * OpenOCD and USB passthrough
    
    * First version of main configuration workflow
    
    * Main configuration update
    
    ---------
    
    Co-authored-by: Radosław Gierwiało <radoslaw.gierwialo@scramjet.org>
    
    * Ignore build/ folder
    
    * README extended by details about workflow with VS Code
    
    * README markdown changes
    
    * Workspace initial configuration
    
    * Runner simple printk over USB example
    
    * README redraft
    
    * Add missing
    
    ---------
    
    Co-authored-by: ErykSol <90617593+ErykSol@users.noreply.github.com>

---
+++ proj/mcu_runner/main.c
@@ -0,0 +1,27 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
+#include <sequence.h>
+
+int main(void)
+{
+	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL))
+	{
+		return 0;
+	}
+
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		k_sleep(K_MSEC(100));
+	}
+
+	while (1) {
+		printk("Hello from runner!\n");
+		run();
+		k_sleep(K_SECONDS(1));
+	}
+}
