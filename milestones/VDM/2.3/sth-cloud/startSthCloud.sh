9df0dfd7c24db284c09d7f4225bdb9b451add896
ErykSol <90617593+ErykSol@users.noreply.github.com>
2023-10-26 15:18:04 +0200

    Feat/clean_after_test (#15)
    * kill script for screen
    
    * readme fix
    
    ---------
    
    Co-authored-by: Eryk Solecki <esolecki@scramjet.org>

--- milestones/VDM/2.3/sth-cloud/startSthCloud.sh
+++ milestones/VDM/2.3/sth-cloud/startSthCloud.sh
@@ -1,1 +1,8 @@
-DEVELOPMENT=1 sth --runtime-adapter process -S ./sth-cloud/sth-cloud-config.json -E -D ./sth-cloud/sequences
+echo "Starting sth"
+screen -d -m -S sth bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S ./sth-cloud/sth-cloud-config.json -E -D ./sth-cloud/sequences"
+sleep 4
+echo "Creating topic"
+si topic create -t "application/octet-stream" throughput
+sleep 1
+echo "Connecting to topic output"
+screen -d -m -S topic bash -c "si topic get throughput -t "application/octet-stream""
--- milestones/VDM/2.3/sth-rpi4/startSthRpi4.sh
+++ milestones/VDM/2.3/sth-rpi4/startSthRpi4.sh
@@ -1,1 +1,5 @@
-DEVELOPMENT=1 sth --runtime-adapter process -S ./sth-rpi4/sth-rpi4-config.json -E -D ./sth-rpi4/sequences
+echo "Starting sth"
+screen -d -m -S sthrpi bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S ./sth-rpi4/sth-rpi4-config.json -E -D ./sth-rpi4/sequences"
+sleep 4
+echo "Starting to send throughput to topic"
+screen -d -m -S throughput bash -c "cat /dev/urandom | pv -q -L 2M | si topic send -t "application/octet-stream" throughput"
