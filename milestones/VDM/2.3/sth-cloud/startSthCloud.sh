83e41500ad3be4e64b008665564893925bc42606
Radosław Gierwiało <radoslaw.gierwialo@scramjet.org>
2023-10-27 12:51:16 +0200

    Fix path errors in bash scripts

--- milestones/VDM/2.3/sth-cloud/startSthCloud.sh
+++ milestones/VDM/2.3/sth-cloud/startSthCloud.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 echo "Starting sth"
 screen -d -m -S sth bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S ./sth-cloud/sth-cloud-config.json -E -D ./sth-cloud/sequences"
-sleep 4
+sleep 10
 echo "Creating topic"
 si topic create -t "application/octet-stream" throughput
 sleep 1
--- milestones/VDM/2.3/sth-rpi4/startSthRpi4.sh
+++ milestones/VDM/2.3/sth-rpi4/startSthRpi4.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+SCRIPT=$(realpath -s "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 echo "Starting sth"
-screen -d -m -S sthrpi bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S ./sth-rpi4/sth-rpi4-config.json -E -D ./sth-rpi4/sequences"
-sleep 4
+screen -d -m -S sthrpi bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S {$SCRIPTPATH}sth-rpi4-config.json -E -D {$SCRIPTPATH}sequences"
+sleep 10
+
 echo "Starting to send throughput to topic"
+si c pr use cloud
 screen -d -m -S throughput bash -c "cat /dev/urandom | pv -q -L 2M | si topic send -t "application/octet-stream" throughput"
+si c pr use default
