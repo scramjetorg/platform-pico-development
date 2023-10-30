8a4b201b3024b058342d392df5cd3e40f1c59be8
Radosław Gierwiało <radoslaw.gierwialo@scramjet.org>
2023-10-30 10:31:31 +0100

    Extra checks before sth starting

--- milestones/VDM/2.3/sth-cloud/startSthCloud.sh
+++ milestones/VDM/2.3/sth-cloud/startSthCloud.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -f /tmp/noise.wav ]
+then
+    echo "[Error] Noise pattern file not found. Copy to: /tmp/noise.wav"
+    exit 3
+fi
+
 echo "Starting sth"
 screen -d -m -S sth bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S ./sth-cloud/sth-cloud-config.json -E -D ./sth-cloud/sequences"
 sleep 10
--- milestones/VDM/2.3/sth-rpi4/startSthRpi4.sh
+++ milestones/VDM/2.3/sth-rpi4/startSthRpi4.sh
@@ -3,8 +3,22 @@
 SCRIPT=$(realpath -s "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+timeout 1 bash -c '< /dev/tcp/192.168.0.5/8000'
+
+if [ "$?" -ne 0 ] 
+then
+    echo "[Error] Pseudo-cloud STH on 192.168.0.5 not responding"
+    exit 2
+fi
+
+if [ ! -f ~/.si/profiles/cloud.json ]
+then
+    echo "[Error] Profile 'cloud.json' for si do not exits"
+    exit 3
+fi
+
 echo "Starting sth"
-screen -d -m -S sthrpi bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S {$SCRIPTPATH}sth-rpi4-config.json -E -D {$SCRIPTPATH}sequences"
+screen -d -m -S sthrpi bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S ${SCRIPTPATH}/sth-rpi4-config.json -E -D ${SCRIPTPATH}/sequences"
 sleep 10
 
 echo "Starting to send throughput to topic"
