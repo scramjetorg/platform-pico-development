b93c3193bc899b15911ca65684962190ff358e3c
Radosław Gierwiało <gierwialo@users.noreply.github.com>
2023-11-13 14:35:33 +0100

    Feat/vdm 2 3 test env (#16)
    * Fix path errors in bash scripts
    
    * Sequence sends request as JSON
    
    * Extra checks before sth starting
    
    * Missing port
    
    * Swapped values for on/off led
    
    * Send fake data from rpi4
    
    * mic_adc sequence clean

--- milestones/VDM/2.3/sth-cloud/startSthCloud.sh
+++ milestones/VDM/2.3/sth-cloud/startSthCloud.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+if [ ! -f /tmp/noise.wav ]
+then
+    echo "[Error] Noise pattern file not found. Copy to: /tmp/noise.wav"
+    exit 3
+fi
+
 echo "Starting sth"
 screen -d -m -S sth bash -c "DEVELOPMENT=1 sth --runtime-adapter process -S ./sth-cloud/sth-cloud-config.json -E -D ./sth-cloud/sequences"
-sleep 4
+sleep 10
 echo "Creating topic"
 si topic create -t "application/octet-stream" throughput
 sleep 1
